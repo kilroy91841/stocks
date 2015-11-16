afd77492d4e17cd4f2ed49c72c01acaa05aac3b5
Ari Golub email = arigolub@gmail.com [credential] helper = osxkeychain [push] default = simple [color] diff = auto status = auto branch = auto interactive = auto ui = true pager = true <arigolub@gmail.com>
2015-11-16 17:48:47 -0500

    woo

--- app.js
+++ app.js
@@ -13,13 +13,28 @@ app.get('/', function (req, res) {
 });
 
 app.post('/', function (req, res) {
-	console.log(req.body);
 	request('http://dev.markitondemand.com/MODApis/Api/v2/Quote/jsonp?callback=myFunction&symbol=' + req.body.text, function (error, response, body) {
 	  if (!error && response.statusCode == 200) {
 	  	body = body.replace("myFunction(","");
 	    body = body.replace(")","");
 	    var stock = JSON.parse(body);
-	  	res.send(stock);
+
+	    var body2 = {};
+	    body2["text"] = stock;
+
+	    var bodyString = JSON.stringify(body2);
+	    console.log(bodyString);
+
+	    request.post({
+	    	url:'https://hooks.slack.com/services/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',
+	    	form: "{\"text\":\"" + stock.Symbol + "-- Last Price: " + stock.LastPrice + ", Change: " + stock.ChangePercent + "\"}"
+	    }, function (error, response, body) {
+	    	console.log("E " + error);
+	    	console.log("R " + response);
+	    	console.log("B " + body);
+	    });	
+
+	  	res.send("k");
 	  }
 	});
 });
