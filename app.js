05566b6c09487aedccaafec15c9e65bbba923b72
Ari Golub <arigolub@gmail.com>
2016-01-26 16:29:11 -0500

    added bowling

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var json = require('JSON');
 var numeral = require('numeral');
 var parseString = require('xml2js').parseString;
 var celebrity = require('./celebrity');
+var moment = require('moment');
 
 var app = express();
 app.set('views', __dirname + '/views');
@@ -55,6 +56,28 @@ app.post('/', function (req, res) {
 		}
 	  }
 	});
+});
+
+app.post('/bowling', function (req, res) {
+	var schedule = [];
+	schedule.push(moment('2016-1-26 20:10:00', 'YYYY-M-DD HH:mm:ss'));
+	schedule.push(moment('2016-2-2 18:30:00', 'YYYY-M-DD HH:mm:ss'));
+	schedule.push(moment('2016-2-9 18:30:00', 'YYYY-M-DD HH:mm:ss'));
+	schedule.push(moment('2016-2-16 20:10:00', 'YYYY-M-DD HH:mm:ss'));
+	schedule.push(moment('2016-2-23 18:30:00', 'YYYY-M-DD HH:mm:ss'));
+	var now = moment();
+	var nextGame = undefined;
+	schedule.forEach(function(s) {
+		if(nextGame == undefined || (now.isBefore(s) && s.isBefore(nextGame))) {
+			nextGame = s;
+		}
+	});
+	var timeTo = now.to(nextGame, true);
+	var attachments = "\"attachments\": [ {\"fallback\" : \"Hope you never see this!\""; 
+	attachments += ", \"color\": \"good\", \"fields\":[ { \"value\":\"BOWLING is in " + timeTo + "\" } ]"
+	attachments += "} ]";
+	res.setHeader("Content-type", "application/json");
+	res.send("{ \"response_type\": \"in_channel\"," + attachments + " }");
 });
 
 app.post('/youlikethat', function (req, res) {
